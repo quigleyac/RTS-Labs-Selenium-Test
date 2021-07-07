d6409ca97fbf2b497ccdad6b8f9bd8e0cc95f31f
quigleyac <71943284+quigleyac@users.noreply.github.com>
2021-07-07 14:06:34 -0400

    Code optimization + Readability
    Did a bit of code optimization before submission

--- TestCases.py
+++ TestCases.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,63 +23,38 @@ I've gone ahead and put this in the form of test cases using python for the 'Big
 '''
 
 class TestWeb(unittest.TestCase):
-    def test_Firefox(self): # The First one i wrote, Check here for Comments
-        driver = webdriver.Firefox()
+    def SeleniumTest(self,driver):
         driver.get("https://www.google.com/")
 
-        SearchField = driver.find_element_by_name("q") 
-        SearchField.send_keys("RTS Labs" + Keys.RETURN)
+        #Honestly, no clue why the Google Search bar's Element is named 'q' but here we are
+        SearchField = driver.find_element_by_name("q")
 
+        SearchField.send_keys("RTS Labs" + Keys.RETURN)
+        #Wait for the google resutls to load, or 30 seconds, the google results URLS are HTML tagged H3, so if we get a list of all of them, then we know the first one is the first result
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'h3')))
         result = driver.find_elements_by_tag_name("h3");
+
         result[0].click()
-        # Wait for page load  
+        # Wait for page load (30 Seconds, or until the browser finds the element labeled 'top', which on the RTS labs site is the green bar at the top of the screen)  
         try:
             WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'top')))
         except TimeoutError:
             print("slow")
 
         self.assertEqual("https://rtslabs.com/",driver.current_url)
-        driver.quit()
+        driver.quit() #Close out of the web page
+
+    def test_Firefox(self): 
+        driver = webdriver.Firefox()
+        TestWeb.SeleniumTest(self,driver)
 
     def test_Chromium(self):
         driver = webdriver.Chrome()
-
-        driver.get("https://www.google.com/")
-
-        SearchField = driver.find_element_by_name("q") 
-        SearchField.send_keys("RTS Labs" + Keys.RETURN)
-
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'h3')))
-        result = driver.find_elements_by_tag_name("h3");
-        result[0].click()
-        # Wait for page load    
-        try:
-            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'top')))
-        except TimeoutError:
-            print("slow")
-        self.assertEqual("https://rtslabs.com/",driver.current_url)
-        driver.quit()
+        TestWeb.SeleniumTest(self,driver)
 
     def test_Edge(self):
         driver = webdriver.Edge()
-
-        driver.get("https://www.google.com/")
-
-        SearchField = driver.find_element_by_name("q") 
-        SearchField.send_keys("RTS Labs" + Keys.RETURN)
-
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'h3')))
-        result = driver.find_elements_by_tag_name("h3");
-        result[0].click()
-        # Wait for page load
-        try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'top')))
-        except TimeoutError:
-            print("slow")
-
-        self.assertEqual("https://rtslabs.com/",driver.current_url)
-        driver.quit()
+        TestWeb.SeleniumTest(self,driver)
 
 if __name__ == '__main__':
     unittest.main()
